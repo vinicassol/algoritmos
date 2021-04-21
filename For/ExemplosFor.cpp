7ab514954bc68d7b7b10e819deed3ddd09d81e8e
Vini Cassol <vinicassol@gmail.com>
2021-04-20 21:11:19 -0300

    Add files via upload

---
+++ For/ExemplosFor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int contador = 1;
+
+  while(contador <=10)
+  {
+    cout << contador << endl;
+    contador++;
+  }
+
+  
+  for(int cont = 1; cont <= 10; cont++)
+  {
+    cout << cont << endl;
+  }
+
+  for(int i=0; i<10; i++)
+    cout << i+1 << endl;
+
+}

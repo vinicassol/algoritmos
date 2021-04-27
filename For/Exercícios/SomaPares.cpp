b7651787b3776b220691e5c34452c7e5f16527a6
Vini Cassol <vinicassol@gmail.com>
2021-04-27 20:01:53 -0300

    Update SomaPares.cpp

--- For/Exercícios/SomaPares.cpp
+++ For/Exercícios/SomaPares.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //Faça  um  algoritmo  que  imprima  todos  os  números  pares  compreendidos  entre  85 e 907 e calcule a soma dos valores impressos 
 int main() 
 {
-  int inicio = 85, fim = 907, soma=0;
+	int inicio = 85, fim = 907, soma=0;
 	for(int cont = inicio; cont <= fim; cont ++)
 	{		
 		if(cont % 2 == 0)
